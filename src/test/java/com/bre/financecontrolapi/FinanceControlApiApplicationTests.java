98f7d4725fcfb87b53f3dd7328a721fa28ba6187
Iochem <brendanunesdeandradeiochem@gmail.com>
2025-05-30 15:10:24 -0300

    Primeiro commit - Subindo API Financeira

---
+++ src/test/java/com/bre/financecontrolapi/FinanceControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bre.financecontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
